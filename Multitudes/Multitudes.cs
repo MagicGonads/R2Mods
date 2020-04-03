344e72e015346f18994d844517c776ee52935489
wildbook <book.wille@gmail.com>
2020-04-03 04:37:36 +0200

    Update Multitudes for the current patch
    This update was in reality done by iDeathHD, who wrote the entire thing for me. I still haven't had time to set up a testing e environment so I'm assuming it works. Thank you!

--- Multitudes/Multitudes.cs
+++ Multitudes/Multitudes.cs
@@ -1,61 +1,66 @@
-﻿using System;
 using BepInEx;
 using BepInEx.Configuration;
-using Mono.Cecil.Cil;
-using MonoMod.Cil;
+using MonoMod.RuntimeDetour;
+using R2API.Utils;
 using RoR2;
 using UnityEngine;
 using UnityEngine.Networking;
-
+ 
 namespace Multitudes
 {
     [BepInDependency("com.bepis.r2api")]
-    [BepInPlugin("dev.wildbook.multitudes", "Multitudes", "1.3.0")]
+    [BepInPlugin("dev.wildbook.multitudes", "Multitudes", "1.4.0")]
     public class Multitudes : BaseUnityPlugin
     {
-        private static ConfigWrapper<int> MultiplierConfig { get; set; }
-
-        public int Multiplier
+        private static ConfigEntry<int> MultiplierConfig { get; set; }
+ 
+        private delegate int RunInstanceReturnInt(Run self);
+ 
+        private static RunInstanceReturnInt origLivingPlayerCountGetter;
+        private static RunInstanceReturnInt origParticipatingPlayerCountGetter;
+ 
+        public static int Multiplier
         {
-            get => MultiplierConfig.Value * (enabled ? 1 : 0);
+            get => MultiplierConfig.Value;
             protected set => MultiplierConfig.Value = value;
         }
-
+ 
         public void Awake()
         {
-            MultiplierConfig = Config.Wrap(
+            MultiplierConfig = Config.Bind(
                 "Game",
                 "Multiplier",
-                "Sets the multiplier for Multitudes.",
-                4);
-
+                4,
+                "Sets the multiplier for Multitudes.");
+ 
             On.RoR2.Console.Awake += (orig, self) =>
             {
-                CommandHelper.RegisterCommands(self);
+                CommandHelper.AddToConsoleWhenReady();
                 orig(self);
             };
-
-            IL.RoR2.Run.FixedUpdate += il =>
-            {
-                var c = new ILCursor(il);
-                c.GotoNext(x => x.MatchCallvirt<Run>("set_livingPlayerCount"));
-                c.EmitDelegate<Func<int, int>>(x => x * Multiplier);
-
-                c.GotoNext(x => x.MatchCallvirt<Run>("set_participatingPlayerCount"));
-                c.EmitDelegate<Func<int, int>>(x => x * Multiplier);
-            };
-
+ 
+            var getLivingPlayerCountHook = new Hook(typeof(Run).GetMethodCached("get_livingPlayerCount"),
+                typeof(Multitudes).GetMethodCached(nameof(GetLivingPlayerCountHook)));
+            origLivingPlayerCountGetter = getLivingPlayerCountHook.GenerateTrampoline<RunInstanceReturnInt>();
+ 
+            var getParticipatingPlayerCount = new Hook(typeof(Run).GetMethodCached("get_participatingPlayerCount"),
+                typeof(Multitudes).GetMethodCached(nameof(GetParticipatingPlayerCountHook)));
+            origParticipatingPlayerCountGetter = getParticipatingPlayerCount.GenerateTrampoline<RunInstanceReturnInt>();
+ 
             Run.onRunStartGlobal += run => { SendMultiplierChat(); };
-
-            On.RoR2.TeleporterInteraction.GetPlayerCountInRadius += (orig, self) => orig(self) * Multiplier;
+ 
+            On.RoR2.HoldoutZoneController.CountPlayersInRadius += (orig, origin, chargingRadiusSqr, teamIndex) => orig(origin, chargingRadiusSqr, teamIndex) * Multiplier;
         }
-
+ 
+        private static int GetLivingPlayerCountHook(Run self) => origLivingPlayerCountGetter(self) * Multiplier;
+        private static int GetParticipatingPlayerCountHook(Run self) => origParticipatingPlayerCountGetter(self) * Multiplier;
+ 
         // Random example command to set multiplier with
         [ConCommand(commandName = "mod_wb_set_multiplier", flags = ConVarFlags.None, helpText = "Lets you pretend to have more friends than you actually do.")]
         private static void CCSetMultiplier(ConCommandArgs args)
         {
             args.CheckArgumentCount(1);
-
+ 
             if (!int.TryParse(args[0], out var multiplier))
             {
                 Debug.Log("Invalid argument.");
@@ -67,14 +72,14 @@ namespace Multitudes
                 SendMultiplierChat();
             }
         }
-
+ 
         private static void SendMultiplierChat()
         {
             // If we're not host, we're not setting it for the current lobby
             // That also means no one cares what our Multitudes is set to
             if (!NetworkServer.active)
                 return;
-
+ 
             Chat.SendBroadcastChat(
                 new Chat.SimpleChatMessage
                 {
@@ -85,7 +90,7 @@ namespace Multitudes
                     }
                 });
         }
-
+ 
         // Random example command to set multiplier with
         [ConCommand(commandName = "mod_wb_get_multiplier", flags = ConVarFlags.None, helpText = "Lets you know what Multitudes' multiplier is set to.")]
         private static void CCGetMultiplier(ConCommandArgs args)
